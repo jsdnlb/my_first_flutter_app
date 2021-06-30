38516a23ed1d4d02b9ac0717beb5d07f247d3683
jsdnlb <danielb.alzate1@gmail.com>
2021-06-29 19:07:37 -0500

    Adding Header AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 // Import description_place
 import 'description_place.dart';
 import 'review_list.dart';
+import 'gradient_place.dart';
+import 'header_appbar.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,11 +29,23 @@ class MyApp extends StatelessWidget {
         ),
         // home: MyHomePage(title: 'Flutter Demo Home Page'),
         home: Scaffold(
-            appBar: AppBar(
+            /*  appBar: AppBar(
               title: Text("Hello world Flutter!"),
-            ),
+            ), */
             // body: new DescriptionPlace("Daniel Buitrago", 4, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
-            body: ReviewList()));
+            // Stack, debajo del elemento va pasar en forma de lista
+            body: Stack(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                DescriptionPlace("Daniel Buitrago", 4,
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+                ReviewList()
+              ],
+            ),
+            HeaderAppBar()
+          ],
+        )));
   }
 }
 
